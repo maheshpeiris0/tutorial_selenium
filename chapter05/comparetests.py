cdf07688c2d6b7f9a374eedfbe60fcab8321fde8
roy <roy_radio@126.com>
2015-01-18 09:48:51 +0800

    advanced techniques

--- chapter05/comparetests.py
+++ chapter05/comparetests.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
