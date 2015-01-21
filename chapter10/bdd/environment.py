351f3ff9caed7e09bb9b41e70ab93c5068b39813
roy <roy_radio@126.com>
2015-01-21 09:44:08 +0800

    integration with CI

---
+++ chapter10/bdd/environment.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+
+
+def before_all(context):
+    context.driver = webdriver.Chrome()
+
+def after_all(context):
+    context.driver.quit()
